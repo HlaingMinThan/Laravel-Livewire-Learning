29919e725bdd58c529e81ad704c26b5be84d1df6
Hlaing Min Than(Faizal) <hlaingminthan92@gmail.com>
2021-04-17 22:43:56 +0630

    vanilla file uploading with laravel

--- database/migrations/2021_04_17_064006_create_posts_table.php
+++ database/migrations/2021_04_17_064006_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->text('body');
+            $table->string('photo')->nullable();
             $table->unsignedBigInteger('user_id');
             $table->timestamps();
         });
--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,34 @@ Route::get('/', function () {
 Route::get('/posts/{post}', function (Post $post) {
     return view('post', compact('post'));
 });
-
-Route::get('/posts/{post}/comment', function (Post $post) {
-    request()->validate([
-        'body' => 'required|min:5'
+Route::get('/posts/{post}/edit', function (Post $post) {
+    return view('editPost', compact('post'));
+});
+Route::patch('/posts/{post}/update', function (Post $post) {
+    $attr = request()->validate([
+        'title' => 'required',
+        'body' => 'required|min:5',
+        'photo' => 'sometimes|image|max:5000',
     ]);
-    $post->comments()->create([
-        'user_id' => 1,
-        'body' => request('body')
+    $post->update([
+        'title' => request('title'),
+        'body' => request('body'),
+        'photo' => request('photo') ? //if request has come with photo
+                    request('photo')->store('images', 'public') : //store it in images folder **store method first arg is store folder name ,sec argument is a disk u can check in filesystems.php**
+                    $post->photo ?? null//if not , check post has photo if it has,override that path if not save null
     ]);
     return back();
-})->name('comments.store');
+})->name('posts.update');
+
+//changed to livewire component
+
+// Route::get('/posts/{post}/comment', function (Post $post) {
+//     request()->validate([
+//         'body' => 'required|min:5'
+//     ]);
+//     $post->comments()->create([
+//         'user_id' => 1,
+//         'body' => request('body')
+//     ]);
+//     return back();
+// })->name('comments.store');
